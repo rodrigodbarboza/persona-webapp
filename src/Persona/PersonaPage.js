4eec879b14cda58adfcb76cdf29f3b92a4f91b2e
Rodrigo Narvaez <rodadev47@gmail.com>
2019-06-05 23:18:37 -0400

    layout almost done I think

---
+++ src/Persona/PersonaPage.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PersonaPage = () => <h2>Persona</h2>
+
+export default PersonaPage
--- src/shared/components/MainRouter/index.js
+++ src/shared/components/MainRouter/index.js
@@ -2,16 +2,22 @@ import React from 'react'
 import { Container } from 'reactstrap'
 import { Switch, Route } from 'react-router'
 import { Home } from 'shared/components'
+import { PersonaPage } from 'Persona'
+
+import './MainRouter.css'
 
 const MainRouter = () => (
-  <Container>
+  <Container className="main-content">
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="/persona" component={Home} />
-      <Route path="/roles" component={Home} />
-      <Route path="/apps" component={Home} />
+      <Route path="/persona" component={PersonaPage} />
+      <Route path="/roles" component={tempRoles} />
+      <Route path="/apps" component={tempApps} />
     </Switch>
   </Container>
 )
+
+const tempRoles = () => <h2>Roles</h2>
+const tempApps = () => <h2>Apps</h2>
 
 export default MainRouter
